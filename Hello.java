7749f993d2e3d2d1e40ba3307e4a4cdb8257dcac
nsjadon29 <nsjadon29@gmail.com>
2023-05-05 15:36:49 +0530

    edited

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-class public Hello{
-	public static void main(){
-		System.out.println("Hello World");
+class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello World GIT");
 }}
